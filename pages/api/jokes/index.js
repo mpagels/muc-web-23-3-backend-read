f8aa144a898d4c6886575c72718031cc6eab8618
Martin Pagels <office@martinpagels.de>
2023-06-20 10:13:40 +0200

    app accept post route in /api/jokes

--- pages/api/jokes/index.js
+++ pages/api/jokes/index.js
@@ -4,6 +4,19 @@ import Joke from "../../../db/models/Joke";
 export default async function handler(request, response) {
   await dbConnect();
 
-  const jokes = await Joke.find();
-  response.status(200).json(jokes);
+  if (request.method === "GET") {
+    const jokes = await Joke.find();
+    response.status(200).json(jokes);
+  }
+
+  if (request.method === "POST") {
+    try {
+      const jokeData = request.body;
+      await Joke.create(jokeData);
+
+      response.status(201).json({ status: "joke created" });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
